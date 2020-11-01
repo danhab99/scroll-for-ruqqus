e8f68beb857726dcd6095d0d1f7d29314c482483
Dan Habot <dan.habot@gmail.com>
2020-10-31 21:55:54 -0400

    Started basic UI

--- App.js
+++ App.js
@@ -1,21 +1,72 @@
+import { NavigationContainer, DrawerActions } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { COLORS, FONTSIZE, SPACE } from './theme'
+import { View, Text } from 'react-native'
+import { IconButton } from './components'
+
+import Feed from './views/feed'
+
+const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function StackTitle(props) {
+  return (
+    <View style={{ 
+      flex: 1, 
+      flexDirection: 'row',
+      width: '100%'
+    }}>
+      <IconButton 
+        icon="menu" 
+        fontsize={4} 
+        onPress={() => props.navigation.toggleDrawer()}
+      />
+      <Text style={{
+        color: COLORS.text,
+        fontSize: FONTSIZE(2),
+        fontWeight: 'bold',
+        marginLeft: SPACE(1)
+      }}>
+        {props.children}
+      </Text>
+    </View>
+  )
+}
+
+function StackNavigator(props) {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: COLORS.backgroundHighlight
+        },
+        headerTitle: ps => <StackTitle {...ps} navigation={props.navigation} />
+      }}
+    >
+      <Stack.Screen
+        name="Home"
+        component={Feed}
+      />
+    </Stack.Navigator>
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StatusBar style="light"/>
+
+      <Drawer.Navigator 
+        initialRouteName="Home"
+        drawerStyle={{
+          backgroundColor: COLORS.backgroundHighlight
+        }}
+      >
+        <Drawer.Screen name="Home" component={StackNavigator} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
