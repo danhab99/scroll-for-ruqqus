0a97b4cbe895863b220a81db5192d45a4eb65392
Dan Habot <dan.habot@gmail.com>
2020-10-31 23:10:59 -0400

    added icon button

--- components.js
+++ components.js
@@ -5,6 +5,15 @@ import { SPACE, FONTSIZE } from './theme'
 
 export function IconButton(props) {
   return (
+    <View style={props.style}>
+      <Pressable onPress={() => props.onPress && props.onPress()}>
+        <Icon name={props.icon} color={props.color || "white"} style={{
+          fontSize: FONTSIZE(props.fontsize || 4)
+        }} />
+      </Pressable>
+    </View>
+  )
+}
     <View style={{
       margin: SPACE(props.margin || 0)
     }}>
