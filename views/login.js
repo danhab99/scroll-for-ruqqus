19164dbf127069564b574e00e9be99903ec732e3
Dan Habot <dan.habot@gmail.com>
2020-11-07 02:40:14 -0500

    Reworking login procedure

--- views/login.js
+++ views/login.js
@@ -1,78 +1,140 @@
 import React from 'react'
-import { Linking, View, Text } from 'react-native'
-import { getAuthURL, Client } from 'ruqqus-js'
-import { ClientID, ClientSecret } from '../secrets'
-import parse from 'url-parse'
+import { View, Text, Image } from 'react-native'
+import Style, { COLORS, FONTS, FONTSIZE, SPACE } from '../theme'
+import { Input, LinkText, Button } from '../components'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 export default class Login extends React.Component {
   constructor(props) {
     super(props)
 
-    this.catchTokens = this.catchTokens.bind(this)
+    this.state = {
+      newServer: {
+        clientID: '',
+        clientSecret: '',
+        domain: ''
+      },
+      servers: []
+    }
+
+    this.onChangeServer = this.onChangeServer.bind(this)
+
   }
 
-  async catchTokens({ url }) {
-    url = parse(url, true)
-    console.log(url)
-
-    if (url.query.state == this.stateToken) {
-      console.log('CODE', url.query.code)
-
-      const client = new Client({
-        id: ClientID,
-        token: ClientSecret,
-        code: url.query.code
-      })
-
-      setTimeout(async () => {
-        debugger
-        // let a1 = await client.guilds.isAvailable('ruqqus')
-        let a2 = await client.guilds.isAvailable('+ruqqus')
-
-        console.log('IS WORKING', a2)
-      }, 3000)
-      
-    }
-    else {
-      // Try login procedure again
-      this.componentWillUnmount()
-      this.componentDidMount()
+  onChangeServer(key) {
+    return value => {
+      this.setState(prev => ({
+        newServer: {
+          ...prev.newServer,
+          [key]: value
+        }
+      }))
     }
   }
 
-  async componentDidMount() {
-    this.listener = Linking.addEventListener('url', this.catchTokens)
-    this.stateToken = Math.random().toString(36).substring(2, 15)
-
-    let returnUrl = await Linking.getInitialURL()
-
-    console.log('RETURN URL', returnUrl)
-
-    let authUrl = getAuthURL({
-      id: ClientID,
-      redirect: `${returnUrl}`,
-      state: this.stateToken,
-      scopes: "identity,create,read,update,delete,vote,guildmaster",
-      permanent: true
-    })
-
-    if (await Linking.canOpenURL(authUrl)) {
-      console.log('OPENING URL', authUrl)
-      Linking.openURL(authUrl)
-    }
-    else {
-      throw new Error('Cannot open auth link')
-    }
+  componentDidMount() {
+    AsyncStorage.getItem('servers', servers => this.setState({
+      servers: JSON.parse(servers) || []
+    }))
   }
 
-  componentWillUnmount() {
-    Linking.removeEventListener('url', this.listener)
+  saveServer() {
+    this.setState(
+      prev => ({servers: prev.servers.concat([this.state.newServer])}),
+      () => AsyncStorage.setItem('servers', JSON.stringify(this.state.servers))
+    )
   }
 
   render() {
     return (
-      <View>
-        <Text style={{color: 'white'}}>Login screen</Text>
+      <View style={Style.view}>
+        <View style={Style.card}>
+          <Text
+            style={{
+              color: COLORS.text,
+              fontSize: FONTSIZE(3)
+            }}
+          >
+            Add New Server
+          </Text>
+
+          <Text style={{color: COLORS.text}}>
+            Until somebody implements proper <LinkText url="https://oauth.net/2/grant-types/implicit/">implicit OAuth</LinkText>, please tap <LinkText url="https://ruqqus.com/settings/apps">here</LinkText> to apply for app keys so you can login.
+          </Text>
+         
+          <Input 
+            label="Client ID"
+            onChangeText={this.onChangeServer('clientID')}
+            autoCompleteType="off"
+            autoCapitalize="none"
+            value={this.state.newServer.clientID}
+          />
+
+          <Input 
+            label="Client Secret"
+            onChangeText={this.onChangeServer('clientSecret')}
+            autoCompleteType="off"
+            autoCapitalize="none"
+            value={this.state.newServer.clientSecret}
+          />
+
+          <Input 
+            label="Domain"
+            onChangeText={this.onChangeServer('domain')}
+            autoCompleteType="off"
+            autoCapitalize="none"
+            value={this.state.newServer.domain}
+          />
+
+          <Button
+            text="Save"
+            onPress={() => this.saveServer()}
+          />
+        </View>
+
+        {this.state.servers.map((server, i) => {
+          return (
+          <View key={`${i}`} style={{
+            ...Style.card,
+            marginTop: SPACE(2)
+          }}>
+            <View style={{
+              display: 'flex',
+              flexDirection: 'row'
+            }}>
+              <Image
+                style={{
+                  width: 30,
+                  height: 30,
+                  borderRadius: 4,
+                  marginRight: SPACE(0.2)
+                }}
+                source={{
+                  uri: `https://${server.domain}/assets/images/logo/favicon.png`
+                }}
+              />
+
+              <Text style={{
+                color: COLORS.text,
+                fontSize: FONTSIZE(2),
+                justifyContent: 'center'
+              }}>
+                {server.domain}
+              </Text>
+            </View>
+            
+
+            <Text style={{
+              color: COLORS.text,
+            }}>
+              {server.clientID}
+            </Text>
+
+            <Button
+              text="Connect account"
+            />
+          </View>)
+        })}
       </View>
     )
   }
