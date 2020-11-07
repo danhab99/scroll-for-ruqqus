2270c9a210243fb7a8c2a050392cf2eef902521d
Dan Habot <dan.habot@gmail.com>
2020-11-07 02:40:46 -0500

    Added feed screen

---
+++ views/feed.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import Style from '../theme'
+import { SubmissionCard } from '../components'
+
+export default function Feed(props) {
+  return (
+    <View style={Style.view}>
+
+    </View>
+  )
+}
