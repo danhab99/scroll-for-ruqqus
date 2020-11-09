82e7a4ce7b1dc9f12ced78f748bed5d0ecf3ee70
Dan Habot <dan.habot@gmail.com>
2020-11-09 11:28:24 -0500

    Got the posts on the feed boi

--- views/feed.js
+++ views/feed.js
@@ -1,12 +1,35 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { ScrollView } from 'react-native'
 import Style from '../theme'
 import { SubmissionCard } from '../components'
+import InitClient from '../init_client'
 
-export default function Feed(props) {
-  return (
-    <View style={Style.view}>
+export default class Feed extends React.Component{
+  constructor(props) {
+    super(props)
 
-    </View>
-  )
+    this.state = {
+      posts: []
+    }
+  } 
+  componentDidMount() {
+    InitClient().then(client => {
+      this._client = client
+
+      client.feeds.frontpage().then(frontpage => {
+        console.log('CLIENT FRONT PAGE', frontpage)
+        this.setState({
+          posts: frontpage.posts
+        })
+      })
+    })
+  }
+  
+  render() {
+    return (
+      <ScrollView style={Style.view}>
+        {this.state.posts.map((post, i) => <SubmissionCard key={`${i}`} post={post} />)}
+      </ScrollView>
+    )
+  }
 }
