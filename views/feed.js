f931b5144843282eac172c07d04c97fcbe781adc
Dan Habot <dan.habot@gmail.com>
2020-11-09 20:40:01 -0500

    Pull to refresh

--- views/feed.js
+++ views/feed.js
@@ -11,20 +11,33 @@ export default class Feed extends React.Component{
     this.state = {
       posts: [],
       page: 1,
-      loadingMore: false
+      loadingMore: false,
+      refreshing: true
     }
   }
 
   componentDidMount() {
-    InitClient().then(client => {
-      this._client = client
+    this.refresh()
+  }
 
-      client.feeds.frontpage().then(frontpage => {
-        console.log('CLIENT FRONT PAGE', frontpage)
+  refresh() {
+    if (this._client) {
+      this._client.feeds.frontpage().then(frontpage => {
         this.setState({
-          posts: frontpage.posts
+          posts: frontpage.posts,
+          refreshing: false
         })
       })
+    }
+    else {
+      InitClient().then(client => {
+        this._client = client
+        this.refresh()
+      })
+    }
+
+    this.setState({
+      refreshing: true
     })
   }
 
@@ -44,34 +57,29 @@ export default class Feed extends React.Component{
   }
   
   render() {
-    if (this.state.posts.length > 0) {
-      return (
-        <View style={{
-          ...Style.view,
-          paddingBottom: 0
-        }}>
-          <FlatList
-            data={this.state.posts}
-            renderItem={props => <SubmissionCard post={props.item}/>}
-            onEndReached={() => this.getMore()}
-            onEndReachedThreshold={0.5}
-            initialNumToRender={26}
-          />
-          {this.state.loadingMore 
-            ? <View style={{position: 'absolute', bottom: 0, width: '100%'}}>
-                <ActivityIndicator 
-                  size="large" 
-                  color={COLORS.primary}
-                />
-              </View> 
-            : null}
-        </View>
-      )
-    }
-    else {
-      return <View style={Style.view}>
-        <ActivityIndicator size="large" color={COLORS.primary}/>
+    return (
+      <View style={{
+        ...Style.view,
+        paddingBottom: 0
+      }}>
+        <FlatList
+          data={this.state.posts}
+          renderItem={props => <SubmissionCard post={props.item}/>}
+          onEndReached={() => this.getMore()}
+          onEndReachedThreshold={0}
+          initialNumToRender={26}
+          onRefresh={() => this.refresh()}
+          refreshing={this.state.refreshing}
+        />
+        {this.state.loadingMore 
+          ? <View style={{position: 'absolute', bottom: 0, width: '100%'}}>
+              <ActivityIndicator 
+                size="large" 
+                color={COLORS.primary}
+              />
+            </View> 
+          : null}
       </View>
-    }
+    )
   }
 }
