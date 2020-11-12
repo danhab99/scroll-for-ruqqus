0df166e7f4821a7a33ef21672d174c0e28b12f9e
Dan Habot <dan.habot@gmail.com>
2020-11-12 17:56:21 -0500

    Shared out stack header + saved posts feed

--- App.js
+++ App.js
@@ -11,6 +11,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage'
 import Feed from './views/feed'
 import Login from './views/login';
 import Comments from './views/comments'
+import Collection from './asyncstorage';
 
 const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
@@ -64,45 +65,91 @@ function StackNavigator(props) {
         />)
       }}
     >
-      <Stack.Screen
-        name="Home"
-        component={Feed}
-        initialParams={{
-          fetch: (client, options) => client.feeds.frontpage(options.page, options.sort)
-        }}
-      />
-
-      <Stack.Screen
-        name="Guild"
-        component={Feed}
-        initialParams={{
-          fetch: (client, options) => client.feeds.guild(options.name, options.page, options.sort),
-          prefix: '+',
-          guildHeader: true
-        }}
-      />
-
-      <Stack.Screen
-        name="Comments"
-        component={Comments}
-        initialParams={{
-          fetch: (client, options) => client.feeds.guild(options.name, options.page, options.sort)
-        }}
-      />
-
-      <Stack.Screen
-        name="User"
-        component={Feed}
-        initialParams={{
-          fetch: (client, options) => client.feeds.user(options.name, options.page, options.sort),
-          prefix: '@'
-        }}
-      />
+      {props.children}
     </Stack.Navigator>
   )
 }
 
+function FeedStackNavigator(props) {
+  return (<StackNavigator {...props}>
+    <Stack.Screen
+      name="Home"
+      component={Feed}
+      initialParams={{
+        fetch: (client, options) => client.feeds.frontpage(options.page, options.sort)
+      }}
+    />
+
+    <Stack.Screen
+      name="Guild"
+      component={Feed}
+      initialParams={{
+        fetch: (client, options) => client.feeds.guild(options.name, options.page, options.sort),
+        prefix: '+',
+        guildHeader: true
+      }}
+    />
+
+    <Stack.Screen
+      name="Comments"
+      component={Comments}
+      initialParams={{
+        fetch: (client, options) => client.feeds.guild(options.name, options.page, options.sort)
+      }}
+    />
+
+    <Stack.Screen
+      name="User"
+      component={Feed}
+      initialParams={{
+        fetch: (client, options) => client.feeds.user(options.name, options.page, options.sort),
+        prefix: '@'
+      }}
+    />
+  </StackNavigator>)
+}
+
+function LoginStackNavigator(props) {
+  return (<StackNavigator {...props}>
+    <Stack.Screen
+      name="Login"
+      component={Login}
+    />
+  </StackNavigator>)
+}
+
+function SavedStackNavigator(props) {
+  return (<StackNavigator {...props}>
+    <Stack.Screen
+      name="Saved" 
+      component={Feed} 
+      initialParams={{
+        fetch: (client, options) => {
+          if (options.page > 1) {
+            return {posts: []}
+          }
+          else {
+            var saved = new Collection('saved')
+            return saved.find({})
+              .then(items => Promise.all(items.map(item => client.posts.fetch(item.pid))))
+              .then(posts => {
+                console.log('SAVED POSTS', posts)
+                return {posts}
+              })
+          }
+        },
+        prefix: '',
+        name: "Saved"
+      }}
+    />
+  </StackNavigator>)
+}
+
 export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
   componentDidMount() {
     // AsyncStorage.clear()
     AsyncStorage.getAllKeys((err, keys) => {
@@ -133,8 +180,9 @@ export default class App extends React.Component {
             }}
             drawerContent={ps => <CustomDrawerContent {...ps} />}
           >
-            <Drawer.Screen name="Home" component={StackNavigator} />
-            <Drawer.Screen name="Login" component={Login} />
+            <Drawer.Screen name="Frontpage" component={FeedStackNavigator} />
+            <Drawer.Screen name="Login" component={LoginStackNavigator} />
+            <Drawer.Screen name="Saved" component={SavedStackNavigator} />
           </Drawer.Navigator>
         </NavigationContainer>
       </View>
