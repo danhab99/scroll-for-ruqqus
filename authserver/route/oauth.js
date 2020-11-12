83049a63ab5383a4fbf84a7c2c9fb577087cb420
Dan Habot <dan.habot@gmail.com>
2020-11-12 00:35:40 -0500

    trying this redirect into app flow

--- authserver/route/oauth.js
+++ authserver/route/oauth.js
@@ -7,7 +7,16 @@ const bodyParser = require('body-parser')
 
 const route = express.Router()
 
+const APPROVED_APP_REDIRECTS = JSON.parse(process.env.APP_REDIRECTS)
+
 route.get('/:id', (req, res) => {
+  if (!APPROVED_APP_REDIRECTS.includes(req.query.redirect)) {
+    res.status(401).json({
+      'oauth_error': 'invalid redirect'
+    })
+    return
+  }
+
   Site.findById(req.params.id).exec((err, site) => {
     if (err || !site) {
       res.status(404).json({err: 'Site does not exist'})
@@ -19,7 +28,10 @@ route.get('/:id', (req, res) => {
       url.searchParams.append('redirect_uri', `https://${process.env.DOMAIN}/auth/${req.params.id}/callback`)
       url.searchParams.append('scope', ["identity", "create", "read", "update", "delete", "vote", "guildmaster"].join(','))
       url.searchParams.append('permanent', 'true')
-      req.query.state && url.searchParams.append('state', req.query.state)
+      url.searchParams.append('state', JSON.stringify({
+        o: req.query.state,
+        r: req.query.redirect
+      }))
 
       res.redirect(url.toString())
     }
@@ -40,20 +52,29 @@ route.get('/:id/callback', (req, res) => {
         }
       })
 
+      var unpackState = JSON.parse(req.query.state)
+
       f.then(rsp => rsp.text())
         .then(text => {
           let r = text
           try {
             r = {
               ...JSON.parse(text),
-              state: req.query.state
+              state: unpackState.o
             }
           }
           catch (e) {
 
           }
           finally {
-            res.send(r)
+            // res.send(r)
+            // res.redirect(`${unpackState.r}?`)
+
+            res.writeHead(301, 'Redirect', {
+              Location: `${unpackState.r}?`
+            })
+            res.write(JSON.stringify(r))
+            res.end()
           }
         })
 
