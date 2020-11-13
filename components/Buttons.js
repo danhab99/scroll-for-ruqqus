975b86838e59f095a991632316fcce0a62436e17
Dan Habot <dan.habot@gmail.com>
2020-11-13 01:57:08 -0500

    fixes

--- components/Buttons.js
+++ components/Buttons.js
@@ -12,7 +12,7 @@ export function IconButton(props) {
       <Pressable 
         onPress={() => props.onPress && props.onPress()} 
         onLongPress={() => props.onLongPress && props.onLongPress()} 
-        delayLongPress={500}
+        delayLongPress={100}
         android_ripple={{
           color: COLORS.primary,
           borderless: true
