81a0ba32f0614ddca6bdc0dde4cecf85f3f56676
Dan Habot <dan.habot@gmail.com>
2020-11-14 15:51:19 -0500

    Replacing async storage to realm

--- android/app/src/main/java/com/scrollforruqqus/MainApplication.java
+++ android/app/src/main/java/com/scrollforruqqus/MainApplication.java
@@ -6,6 +6,7 @@ import android.net.Uri;
 
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import io.realm.react.RealmReactPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.facebook.react.ReactInstanceManager;
