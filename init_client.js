3012e37be2632fefd44e25cea0aadc4baa46396a
Dan Habot <dan.habot@gmail.com>
2020-11-17 22:21:07 -0500

    memoized init client

--- init_client.js
+++ init_client.js
@@ -2,38 +2,46 @@ import Collection, { Value } from './asyncstorage'
 import { Client } from 'ruqqus-js'
 
 export default async function InitClient() {
-  let accounts = new Collection('accounts')
-  let servers = new Collection('servers')
-  let activeID = await Value.getValue('activeAccount')
-
-  if (!activeID) {
-    throw new Error('Requires login')
+  if (global.ruqqClient) {
+    console.log('RETURNING GLOBAL CLIENT')
+    return global.ruqqClient
   }
-
-  let account = await accounts.findById(activeID)
-  let server = await servers.findById(account.serverID)
-  let client = new Client({
-    domain: server.domain,
-    refresh: account.keys.refresh,
-    accessToken: account.keys.access,
-    id: server.clientID,
-    token: server.clientSecret
-  })
-
-  await Promise.all([
-    new Promise(ready => {
-      client.on("login", () => {
-        console.log('Logged in')
-        ready()
-      })
-    }),
-    new Promise(ready => {
-      client.on('refresh', () => {
-        console.log('Refreshed tokens')
-        ready()
-      })
+  else {
+    console.log('INITALIZING NEW CLIENT')
+    let accounts = new Collection('accounts')
+    let servers = new Collection('servers')
+    let activeID = await Value.getValue('activeAccount')
+  
+    if (!activeID) {
+      throw new Error('Requires login')
+    }
+  
+    let account = await accounts.findById(activeID)
+    let server = await servers.findById(account.serverID)
+    let client = new Client({
+      domain: server.domain,
+      refresh: account.keys.refresh,
+      accessToken: account.keys.access,
+      id: server.clientID,
+      token: server.clientSecret
     })
-  ])
-
-  return client
+  
+    await Promise.all([
+      new Promise(ready => {
+        client.on("login", () => {
+          console.log('Logged in')
+          ready()
+        })
+      }),
+      new Promise(ready => {
+        client.on('refresh', () => {
+          console.log('Refreshed tokens')
+          ready()
+        })
+      })
+    ])
+    
+    global.ruqqClient = client
+    return client
+  }
 }
