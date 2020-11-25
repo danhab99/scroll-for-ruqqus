2acfb0d9d8857a8fae68e2d99f1ec1c0e6e9e5c3
Dan Habot <dan.habot@gmail.com>
2020-11-25 13:44:23 -0500

    Installed svg loader

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,24 @@
-module.exports = {
-  transformer: {
-    assetPlugins: ['expo-asset/tools/hashAssetFiles'],
-  },
-};
+// module.exports = {
+//   transformer: {
+//     assetPlugins: ['expo-asset/tools/hashAssetFiles'],
+//   },
+// };
+// // https://stackoverflow.com/questions/55763428/react-native-error-enospc-system-limit-for-number-of-file-watchers-reached
+
+const { getDefaultConfig } = require("metro-config");
+
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts, assetExts }
+  } = await getDefaultConfig();
+  return {
+    transformer: {
+      babelTransformerPath: require.resolve("react-native-svg-transformer"),
+      assetPlugins: ['expo-asset/tools/hashAssetFiles']
+    },
+    resolver: {
+      assetExts: assetExts.filter(ext => ext !== "svg"),
+      sourceExts: [...sourceExts, "svg"]
+    }
+  };
+})();
