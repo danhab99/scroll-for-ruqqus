8f83a18faa2f1c285cecdfc94523384b35954f97
Dan Habot <dan.habot@gmail.com>
2020-11-26 13:52:48 -0500

    fixed safe area context

--- android/app/src/main/java/com/danhab99/scrollforruqqus/MainApplication.java
+++ android/app/src/main/java/com/danhab99/scrollforruqqus/MainApplication.java
@@ -12,6 +12,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import com.danhab99.scrollforruqqus.generated.BasePackageList;
+import com.th3rdwave.safeareacontext.SafeAreaContextPackage;
 
 import org.unimodules.adapters.react.ReactAdapterPackage;
 import org.unimodules.adapters.react.ModuleRegistryAdapter;
@@ -43,6 +44,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       List<ReactPackage> packages = new PackageList(this).getPackages();
       packages.add(new ModuleRegistryAdapter(mModuleRegistryProvider));
+      packages.add(new SafeAreaContextPackage());
       return packages;
     }
 
