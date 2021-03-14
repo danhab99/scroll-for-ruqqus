61a1b895d654461dbe2f8d1c6bd3008fed98fb8d
Dan Habot <dan.habot@gmail.com>
2021-03-14 01:06:03 -0500

    no more global values, we're using contexts

--- init_client.js
+++ init_client.js
@@ -1,56 +1,50 @@
-import Collection, {Value} from './asyncstorage';
+import Collection from './asyncstorage';
 import {Client} from 'ruqqus-js';
 
 export default async function InitClient(opts) {
-  if (global.ruqqClient && !opts) {
-    console.log('RETURNING GLOBAL CLIENT');
-    return global.ruqqClient;
+  console.log('INITALIZING NEW CLIENT');
+  let c = {};
+
+  if (typeof opts == 'object') {
+    c = opts;
   } else {
-    console.log('INITALIZING NEW CLIENT');
-    let c = {};
+    let accounts = new Collection('accounts');
+    let servers = new Collection('servers');
+    let activeID = opts;
 
-    if (opts) {
-      c = opts;
-    } else {
-      let accounts = new Collection('accounts');
-      let servers = new Collection('servers');
-      let activeID = await Value.getValue('activeAccount');
-
-      if (!activeID) {
-        throw new Error('Requires login');
-      }
-
-      let account = await accounts.findById(activeID);
-      let server = await servers.findById(account.serverID);
-
-      c = {
-        domain: server.domain,
-        refresh_token: account.keys.refresh,
-        access_token: account.keys.access,
-        id: server._id,
-        token: server.clientSecret,
-        auth_domain: 'sfroa.danhab99.xyz',
-      };
+    if (!activeID) {
+      throw new Error('Requires login');
     }
 
-    let client = new Client(c);
+    let account = await accounts.findById(activeID);
+    let server = await servers.findById(account.serverID);
 
-    await Promise.all([
-      new Promise((ready) => {
-        client.on('login', () => {
-          console.log('Logged in');
-          ready();
-        });
-      }),
-      new Promise((ready) => {
-        client.on('refresh', () => {
-          console.log('Refreshed tokens');
-          ready();
-        });
-      }),
-    ]);
-
-    global.ruqqClient = client;
-    return client;
+    c = {
+      domain: server.domain,
+      refresh_token: account.keys.refresh,
+      access_token: account.keys.access,
+      id: server._id,
+      token: server.clientSecret,
+      auth_domain: 'sfroa.danhab99.xyz',
+    };
   }
+
+  let client = new Client(c);
+
+  await Promise.all([
+    new Promise((ready) => {
+      client.on('login', () => {
+        console.log('Logged in');
+        ready();
+      });
+    }),
+    new Promise((ready) => {
+      client.on('refresh', () => {
+        console.log('Refreshed tokens');
+        ready();
+      });
+    }),
+  ]);
+
+  return client;
 }
