71605b34ed03eb564ef32f2d00138f9c09297476
Dan Habot <dan.habot@gmail.com>
2021-03-14 00:31:10 -0500

    refactored to function

--- views/submit.js
+++ views/submit.js
@@ -1,133 +1,79 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, ActivityIndicator} from 'react-native';
 import InitClient from '../init_client';
 import Style, {COLORS, SPACE} from '../theme';
 import Input from '../components/Input';
 import {Button} from '../components/Buttons';
+import {useRuqqusClient} from '../components/ruqqus-client';
+import {useNavigation} from '@react-navigation/core';
 
-export default class Submit extends React.Component {
-  constructor(props) {
-    super(props);
+export default function Submit(props) {
+  const client = useRuqqusClient();
+  const navigation = useNavigation();
 
-    this.state = {
-      ready: false,
-      submitting: false,
-      form: {
-        board: '',
-        url: '',
-        title: '',
-        body: '',
-      },
-    };
+  const [ready, setReady] = useState(false);
+  const [submitting, setSubmitting] = useState();
+  const [board, setBoard] = useState('');
+  const [url, setUrl] = useState('');
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
 
-    this.onChange = this.onChange.bind(this);
-  }
+  const submit = () => {
+    setSubmitting(true);
 
-  componentDidMount() {
-    InitClient()
-      .then((client) => {
-        this._client = client;
-        this.setState({ready: true});
-      })
-      .catch(() => {
-        this.props.navigation.navigate('Login');
-      });
-  }
+    client.submitPost(board, title, url, body).then((post) => {
+      setSubmitting(false);
+      navigation.navigate('Comments', {post});
+    });
+  };
 
-  onChange(key) {
-    return (value) => {
-      this.setState((prev) => ({
-        form: {
-          ...prev.form,
-          [key]: value,
-        },
-      }));
-    };
-  }
+  const disabled = !(
+    this.state.ready && Object.values(this.state.form).every((x) => x !== '')
+  );
 
-  submit() {
-    this.setState(
-      {
-        submitting: true,
-      },
-      () => {
-        this._client
-          .submitPost(
-            this.state.form.board,
-            this.state.form.title,
-            this.state.form.url,
-            this.state.form.body,
-          )
-          .then((post) => {
-            console.log('SUBMITTED POST', post);
-            this.setState(
-              {
-                submitting: false,
-              },
-              () => {
-                this.props.navigation.navigate('Comments', {
-                  post,
-                  navigation: this.props.navigation,
-                });
-              },
-            );
-          });
-      },
-    );
-  }
+  return (
+    <View style={Style.view}>
+      <Input
+        label="Board"
+        onChangeText={this.onChange('board')}
+        autoCompleteType="off"
+        autoCapitalize="none"
+        value={this.state.form.board}
+      />
 
-  get disabled() {
-    // return !(this.state.ready && this.state.form.board !== '' && this.state.form.title !== '' && (this.state.form.body !== '' || this.state.form.url !== ''))
-    return !(
-      this.state.ready && Object.values(this.state.form).every((x) => x !== '')
-    );
-  }
+      <Input
+        label="Title"
+        onChangeText={this.onChange('title')}
+        value={this.state.form.title}
+      />
 
-  render() {
-    return (
-      <View style={Style.view}>
-        <Input
-          label="Board"
-          onChangeText={this.onChange('board')}
-          autoCompleteType="off"
-          autoCapitalize="none"
-          value={this.state.form.board}
-        />
+      <Input
+        label="URL"
+        onChangeText={this.onChange('url')}
+        autoCompleteType="off"
+        autoCapitalize="none"
+        value={this.state.form.url}
+      />
 
-        <Input
-          label="Title"
-          onChangeText={this.onChange('title')}
-          value={this.state.form.title}
-        />
+      <Input
+        label="Body"
+        onChangeText={this.onChange('body')}
+        value={this.state.form.body}
+        multiline
+      />
 
-        <Input
-          label="URL"
-          onChangeText={this.onChange('url')}
-          autoCompleteType="off"
-          autoCapitalize="none"
-          value={this.state.form.url}
-        />
+      <Button
+        disabled={this.disabled}
+        text="Submit"
+        style={{
+          marginTop: SPACE(1),
+        }}
+        onPress={() => this.submit()}
+      />
 
-        <Input
-          label="Body"
-          onChangeText={this.onChange('body')}
-          value={this.state.form.body}
-          multiline
-        />
-
-        <Button
-          disabled={this.disabled}
-          text="Submit"
-          style={{
-            marginTop: SPACE(1),
-          }}
-          onPress={() => this.submit()}
-        />
-
-        {this.state.submitting ? (
-          <ActivityIndicator size="large" color={COLORS.primary} />
-        ) : null}
-      </View>
-    );
-  }
+      {this.state.submitting ? (
+        <ActivityIndicator size="large" color={COLORS.primary} />
+      ) : null}
+    </View>
+  );
 }
