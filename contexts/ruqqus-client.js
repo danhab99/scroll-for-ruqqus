901510c86b91b4334863f1581d925b5b4c4834a0
Dan Habot <dan.habot@gmail.com>
2021-03-14 01:10:42 -0500

    client provider inits itself

--- contexts/ruqqus-client.js
+++ contexts/ruqqus-client.js
@@ -1,11 +1,27 @@
-import React, {createContext, useContext} from 'react';
-import {useState} from 'react';
+import React, {createContext, useContext, useState, useEffect} from 'react';
+import {useValue} from './storage-context';
 
 const ClientContext = createContext();
 
 export function RuqqusClientProvider(props) {
+  const activeID = useValue('activeID');
+  const navigation = useNavigation();
+
+  const [ready, setReady] = useState(false);
+  const [client, setClient] = useState();
+
+  useEffect(async () => {
+    if (!activeID) {
+      navigation.navigate('Login');
+      return;
+    }
+
+    setClient(InitClient(activeID));
+    setReady(true);
+  }, [activeID]);
+
   return (
-    <ClientContext.Provider value={props.client}>
+    <ClientContext.Provider value={{ready, client}}>
       {props.children}
     </ClientContext.Provider>
   );
@@ -15,11 +31,11 @@ export function useRuqqusClient() {
   return useContext(ClientContext);
 }
 
-export function useQuery(edge, opts) {
-  const client = useRuqqusClient();
-  const [loading, setLoading] = useState(false);
-  const [data, setData] = useState();
-  const [error, setError] = useState();
+// export function useQuery(edge, opts) {
+//   const client = useRuqqusClient();
+//   const [loading, setLoading] = useState(false);
+//   const [data, setData] = useState();
+//   const [error, setError] = useState();
 
-  return {loading, data, error, client};
-}
+//   return {loading, data, error, client};
+// }
