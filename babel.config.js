89c1a181430dba20931630a57a05d48dd1290d5d
Dan Habot <dan.habot@gmail.com>
2021-04-08 23:26:25 -0400

    reexported contexts

--- babel.config.js
+++ babel.config.js
@@ -18,6 +18,7 @@ module.exports = {
         ],
         alias: {
           '@react-ruqqus': './react-ruqqus',
+          '@contexts': './contexts',
         },
       },
     ],
---
+++ contexts/index.ts
@@ -0,0 +1,17 @@
+import {ValueProvider, useSetValue, useValue} from './storage-context';
+import {
+  ThemeConsumer,
+  ThemeProvider,
+  useStyle,
+  useTheme,
+} from './theme-context';
+
+export {
+  ThemeConsumer,
+  ThemeProvider,
+  useStyle,
+  useTheme,
+  ValueProvider,
+  useSetValue,
+  useValue,
+};
