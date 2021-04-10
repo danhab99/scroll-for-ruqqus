fa4fed8d8094ae3740310cb1b9316d1f3d25aa6e
Dan Habot <dan.habot@gmail.com>
2021-04-10 19:45:25 -0400

    added refresh function

--- react-ruqqus/useFeed.tsx
+++ react-ruqqus/useFeed.tsx
@@ -2,11 +2,11 @@ import {useState, useEffect} from 'react';
 import {UseFetchOpts} from './useFetch';
 import {useRuqqusFetch} from './useRuqqusFetch';
 
-export function useFeed(edge: string, args: UseFetchOpts | undefined) {
+export function useFeed(edge: string, args?: UseFetchOpts) {
   const [posts, setPosts] = useState<object[]>();
   const [page, setPage] = useState(1);
 
-  const {loading, body} = useRuqqusFetch(`/api/v1/${edge}/listing`, {
+  const {loading, body} = useRuqqusFetch(`${edge}/listing`, {
     args: {
       ...args,
       page,
@@ -19,5 +19,10 @@ export function useFeed(edge: string, args: UseFetchOpts | undefined) {
     }
   }, [page]);
 
-  return {loading, posts, nextPage: setPage((x) => x + 1)};
+  return {
+    loading,
+    posts,
+    nextPage: () => setPage((x) => x + 1),
+    refresh: () => setPage(0),
+  };
 }
