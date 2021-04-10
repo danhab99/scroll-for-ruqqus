672b86f648f7fb028eff27a1f5b23608a6063869
Dan Habot <dan.habot@gmail.com>
2021-04-10 02:04:08 -0400

    better clear function

--- react-ruqqus/useOnWebviewClear.tsx
+++ react-ruqqus/useOnWebviewClear.tsx
@@ -1,13 +1,22 @@
 import {useEffect} from 'react';
 import {useRuqqusClient} from './useRuqqusClient';
 
-export function useOnWebviewClear(clear: () => void) {
+interface UserData {
+  access_token: string;
+  refresh_token: string;
+  // username: string;
+}
+
+export function useOnWebviewClear(clear: (user: UserData) => void) {
   const client = useRuqqusClient();
 
   useEffect(() => {
     if (client?.access_token) {
       debugger;
-      clear();
+      clear({
+        access_token: client.access_token,
+        refresh_token: client.refresh_token,
+      });
     }
   }, [client]);
 }
