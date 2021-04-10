65aa421b64f86e91b9b79f18056e64fab8675c47
Dan Habot <dan.habot@gmail.com>
2021-04-10 02:04:17 -0400

    initial login redesign

--- views/roa_login.tsx
+++ views/roa_login.tsx
@@ -1,221 +1,139 @@
 import React, {useState, useEffect, useRef} from 'react';
 import {View, Text, Linking, ScrollView, ActivityIndicator} from 'react-native';
-import Style, {BODYTEXT, COLORS, FONTSIZE, SPACE} from '../theme';
 import {Button, IconButton} from '../components/Buttons';
-import {WebView} from 'react-native-webview';
-import Collection, {Value} from '../asyncstorage';
-import {useNavigation} from '@react-navigation/core';
-import {useSetValue} from '../contexts/storage-context';
+import {useSetValue, useStyle, useTheme} from '@contexts';
+import {
+  AuthSiteWebview,
+  useAuthSites,
+  useIdentity,
+  useOnWebviewClear,
+} from '@react-ruqqus';
+import {useValue} from '@contexts';
 
-const CAPTURE_TOKENS = `
-window.ReactNativeWebView.postMessage(document.body.innerText)
-`;
+interface Site {
+  _id: string;
+  clientID: string;
+  domain: string;
+  name: string;
+}
+
+interface Account {
+  username: string;
+  access_token: string;
+  refresh_token: string;
+  siteID: string;
+}
 
 export default function ROALogin() {
-  const navigation = useNavigation();
-  const setValue = useSetValue();
+  const style = useStyle();
+  const theme = useTheme();
+  const accounts = useValue<Account[]>('accounts');
+  const setAccounts = useSetValue<Account[]>('accounts');
+  const {loading, sites, getAuthURL} = useAuthSites();
+  const {body: identity, resp} = useIdentity();
 
-  const [sites, setSites] = useState<Object[]>([]);
-  const [connectTo, setConnectTo] = useState<string>();
   const [connecting, setConnecting] = useState(false);
-  const [logginIn, setLoggingIn] = useState(false);
-  const [loading, setLoading] = useState(false);
-
-  const webViewRef = useRef<any>();
-  const tokenLock = useRef<boolean>();
-
-  const accounts = new Collection('accounts');
-  const servers = new Collection('servers');
-
-  const fetchSites = () => {
-    setLoading(true);
-
-    fetch('https://sfroa.danhab99.xyz/sites')
-      .then((d) => d.json())
-      .then((sites) =>
-        Promise.all(
-          sites.map((site: any) =>
-            servers
-              .findOne({_id: site._id})
-              .then((server: any) => (server ? server : servers.create(site))),
-          ),
-        ),
-      )
-      .then((servers) =>
-        Promise.all(
-          servers.map((server: any) =>
-            accounts.find({serverID: server._id}).then((accounts: any) => {
-              server.accounts = accounts;
-              return server;
-            }),
-          ),
-        ),
-      )
-      .then((sites) => {
-        setSites(sites);
-        setLoading(false);
-      });
-  };
-
-  useEffect(() => fetchSites(), []);
+  const [siteID, setSiteID] = useState<string>();
+  const [newTokens, setNewTokens] = useState<any>();
 
   const connectAccount = (id: string) => {
-    fetch(`https://sfroa.danhab99.xyz/auth/${id}?state=${id}`).then(({url}) => {
-      setConnectTo(url);
-      setConnecting(sites.find((x: any) => x._id == id) ? true : false);
-    });
+    setConnecting(true);
+    getAuthURL(id);
+    setSiteID(id);
   };
 
-  const catchTokens = (msg) => {
-    console.log('WEBVIEW', msg);
-    let data = msg.nativeEvent.data;
+  useOnWebviewClear((user) => {
+    console.log(user);
+    setConnecting(false);
+    setNewTokens(user);
+  });
 
-    if (!tokenLock.current) {
-      try {
-        data = JSON.parse(data);
-
-        if (data['access_token']) {
-          console.log('TOKENS', data);
-          tokenLock.current = true;
-
-          setLoggingIn(true);
-
-          // InitClient({
-          //   ...connecting,
-          //   id: connecting._id,
-          //   auth_domain: `sfroa.danhab99.xyz`,
-          //   access_token: data.access_token,
-          //   refresh_token: data.refresh_token,
-          // }).then((client) => {
-          //   accounts.create({
-          //     serverID: connecting._id,
-          //     username: client.user.username,
-          //     keys: {
-          //       access: data.access_token,
-          //       refresh: data.refresh_token,
-          //     },
-          //   });
-          //   setLoggingIn(false);
-          //   setConnecting(false);
-          //   tokenLock.current = false;
-          //   fetchSites();
-          // });
-        }
-      } catch (e) {
-        console.error('UNABLE TO CATCH TOKEN', e);
+  useEffect(() => {
+    if (newTokens && identity && siteID && resp) {
+      if (resp.ok) {
+        debugger;
+        setAccounts((accounts = []) => [
+          ...accounts,
+          {
+            ...newTokens,
+            siteID,
+            username: identity.username,
+          },
+        ]);
       }
     }
-  };
+  }, [newTokens, identity, siteID, resp]);
 
-  const deleteAccount = (id: any) =>
-    accounts.delete({_id: id}).then(() => fetchSites());
+  useEffect(() => console.log('ACCOUNTS', accounts), [accounts]);
 
-  const pickAccount = (id: any) => {
-    setValue(id, 'activeID');
-    navigation.navigate('Frontpage');
-  };
-
-  if (connecting) {
-    return (
-      <WebView
-        ref={webViewRef}
-        source={{
-          uri: connectTo || '',
-        }}
-        injectedJavaScript={CAPTURE_TOKENS}
-        injectJavaScript={CAPTURE_TOKENS}
-        onMessage={(msg) => catchTokens(msg)}
-        onNavigationStateChange={() => {
-          webViewRef.current?.injectJavaScript(CAPTURE_TOKENS);
-        }}
-        renderLoading={() => (
-          <ActivityIndicator size="large" color={COLORS.primary} />
-        )}
-        startInLoadingState={true}
-      />
-    );
-  } else if (loading) {
-    return (
-      <View style={Style.view}>
-        <ActivityIndicator size="large" color={COLORS.primary} />
-      </View>
-    );
-  } else if (!loading && sites?.length <= 0) {
-    return (
-      <View>
-        <Text style={{...BODYTEXT, fontSize: FONTSIZE(2)}}>
-          Error: No sites have been retrieved
-        </Text>
-
-        <Button text="Try again" onPress={() => fetchSites()} />
-      </View>
-    );
+  if (loading) {
+    <View style={style?.view}>
+      <ActivityIndicator color={theme?.Colors.primary} size="large" />;
+    </View>;
+  } else if (connecting) {
+    return <AuthSiteWebview />;
   } else {
     return (
-      <ScrollView style={Style.view}>
-        {loggingIn ? (
-          <View>
-            <ActivityIndicator color={COLORS.primary} size="large" />
-          </View>
-        ) : null}
-
-        {sites.map((site, i) => (
-          <View key={`${i}`} style={Style.card}>
+      <ScrollView style={style?.view}>
+        {sites?.map((site: Site, i: number) => (
+          <View key={`${i}`} style={style?.card}>
             <Text
               style={{
-                color: COLORS.text,
-                fontSize: FONTSIZE(2),
+                color: theme?.Colors?.text,
+                fontSize: theme?.FontSize.get?.(2),
                 fontWeight: 'bold',
               }}>
               {site.domain}
             </Text>
 
             <View>
-              {(site.accounts || []).map((account, i) => (
-                <View
-                  key={`${i}`}
-                  style={{
-                    marginTop: SPACE(1),
-                    display: 'flex',
-                    flexDirection: 'row',
-                    justifyContent: 'flex-end',
-                    alignContent: 'center',
-                    alignItems: 'center',
-                  }}>
-                  <Text
+              {(Array.isArray(accounts) ? accounts : [])
+                .filter((x) => x.siteID === site._id)
+                .map((account, i) => (
+                  <View
+                    key={`${i}`}
                     style={{
-                      color: COLORS.text,
-                      fontWeight: 'bold',
-                      textAlign: 'center',
-                      margin: 'auto',
+                      marginTop: theme?.Space.get?.(1),
+                      display: 'flex',
+                      flexDirection: 'row',
+                      justifyContent: 'flex-end',
                       alignContent: 'center',
-                      fontSize: FONTSIZE(1.5),
-                      marginRight: SPACE(1),
+                      alignItems: 'center',
                     }}>
-                    @{account.username}
-                  </Text>
+                    <Text
+                      style={{
+                        color: theme?.Colors.text,
+                        fontWeight: 'bold',
+                        textAlign: 'center',
+                        margin: 'auto',
+                        alignContent: 'center',
+                        fontSize: theme?.FontSize.get?.(1.5),
+                        marginRight: theme?.Space.get?.(1),
+                      }}>
+                      @{account.username}
+                    </Text>
 
-                  <Button
-                    text="Login"
-                    style={{
-                      marginRight: SPACE(1),
-                    }}
-                    onPress={() => pickAccount(account._id)}
-                  />
+                    <Button
+                      text="Login"
+                      style={{
+                        marginRight: theme?.Space.get?.(1),
+                      }}
+                      // onPress={() => pickAccount(account._id)}
+                    />
 
-                  <IconButton
-                    icon="delete"
-                    onPress={() => deleteAccount(account._id)}
-                  />
-                </View>
-              ))}
+                    <IconButton
+                      name="delete"
+                      // onPress={() => deleteAccount(account._id)}
+                    />
+                  </View>
+                ))}
             </View>
 
             <Button
               text="Connect an account"
               onPress={() => connectAccount(site._id)}
               style={{
-                marginTop: SPACE(1),
+                marginTop: theme?.Space.get?.(1),
               }}
             />
           </View>
@@ -223,4 +141,6 @@ export default function ROALogin() {
       </ScrollView>
     );
   }
+
+  return <View></View>;
 }
