39e585192bec088c58431dc84078c878e897bfb4
Dan Habot <dan.habot@gmail.com>
2021-04-10 23:56:52 -0400

    this was causing a react bug

--- react-ruqqus/useRuqqusFetch.ts
+++ react-ruqqus/useRuqqusFetch.ts
@@ -3,15 +3,9 @@ import {useRuqqusClient} from './useRuqqusClient';
 
 export function useRuqqusFetch(edge: string, opts: UseFetchOpts = {}) {
   const client = useRuqqusClient();
-  return client?.access_token
-    ? useFetch(
-        client.domain,
-        '/api/v1/' + edge,
-        Object.assign(opts, {access_token: client.access_token}),
-      )
-    : {
-        loading: false,
-        resp: new Error('No access token specified'),
-        body: new Error('No access token specified'),
-      };
+  return useFetch(
+    client.domain,
+    '/api/v1/' + edge,
+    Object.assign(opts, {access_token: client.access_token}),
+  );
 }
