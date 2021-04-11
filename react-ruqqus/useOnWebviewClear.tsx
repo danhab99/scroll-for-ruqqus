5b1fbba7f1afd7372b034a012573c99b0c1ee458
Dan Habot <dan.habot@gmail.com>
2021-04-10 21:43:14 -0400

    get identity

--- react-ruqqus/useOnWebviewClear.tsx
+++ react-ruqqus/useOnWebviewClear.tsx
@@ -1,10 +1,12 @@
 import {useEffect} from 'react';
+import {fetcher} from './fetcher';
+import {RuqqusUser} from './types';
 import {useRuqqusClient} from './useRuqqusClient';
 
 interface UserData {
   access_token: string;
   refresh_token: string;
-  // username: string;
+  user: RuqqusUser;
 }
 
 export function useOnWebviewClear(clear: (user: UserData) => void) {
@@ -13,9 +15,15 @@ export function useOnWebviewClear(clear: (user: UserData) => void) {
   useEffect(() => {
     if (client?.access_token) {
       debugger;
-      clear({
+
+      fetcher(client.domain, '/api/v1/identity', {
         access_token: client.access_token,
-        refresh_token: client.refresh_token,
+      }).then((resp) => {
+        clear({
+          access_token: client.access_token,
+          refresh_token: client.refresh_token,
+          user: resp.body as RuqqusUser,
+        });
       });
     }
   }, [client]);
