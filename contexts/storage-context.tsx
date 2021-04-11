bf4cda5d699891d112359a4617dc28430dc39415
Dan Habot <dan.habot@gmail.com>
2021-04-11 17:30:49 -0400

    fix

--- contexts/storage-context.tsx
+++ contexts/storage-context.tsx
@@ -5,6 +5,7 @@ import {
   writeFile,
   readFile,
   readdir,
+  mkdir,
 } from 'react-native-fs';
 import * as _ from 'lodash';
 
@@ -24,38 +25,44 @@ export function ValueProvider(props: ContextChildrenProps) {
 
   const write = () => {
     console.log('VALUE WRITE', value);
-    Object.entries(value).forEach(([file, content]) =>
-      writeFile(
-        `${DocumentDirectoryPath}/vars/${file}.json`,
-        JSON.stringify(content),
+    mkdir(DocumentDirectoryPath + '/vars/').then(() =>
+      Object.entries(value).forEach(([file, content]) =>
+        writeFile(
+          `${DocumentDirectoryPath}/vars/${file}.json`,
+          JSON.stringify(content),
+        ),
       ),
     );
   };
 
   const read = () => {
-    readdir(DocumentDirectoryPath)
-      .then((files: string[]) => files.filter((x) => x.includes('json')))
-      .then((files: string[]) => {
-        console.log('VALUE READ', files);
-        return Promise.all(
-          files.map((file) =>
-            readFile(DocumentDirectoryPath + '/vars/' + file).then((data) => ({
-              file,
-              data: JSON.parse(data),
-            })),
-          ),
-        );
-      })
-      .then((files: {file: string; data: any}[]) => {
-        console.log('VALUE READ FILES', files);
-        let o = files.reduce(
-          (prev, curr) =>
-            Object.assign(prev, {[curr.file.slice(0, -5)]: curr.data}),
-          {},
-        );
+    mkdir(DocumentDirectoryPath + '/vars/').then(() =>
+      readdir(DocumentDirectoryPath + '/vars/')
+        .then((files: string[]) => files.filter((x) => x.includes('json')))
+        .then((files: string[]) => {
+          console.log('VALUE READ', files);
+          return Promise.all(
+            files.map((file) =>
+              readFile(DocumentDirectoryPath + '/vars/' + file).then(
+                (data) => ({
+                  file,
+                  data: JSON.parse(data),
+                }),
+              ),
+            ),
+          );
+        })
+        .then((files: {file: string; data: any}[]) => {
+          console.log('VALUE READ FILES', files);
+          let o = files.reduce(
+            (prev, curr) =>
+              Object.assign(prev, {[curr.file.slice(0, -5)]: curr.data}),
+            {},
+          );
 
-        setValue(o);
-      });
+          setValue(o);
+        }),
+    );
   };
 
   useEffect(() => {
