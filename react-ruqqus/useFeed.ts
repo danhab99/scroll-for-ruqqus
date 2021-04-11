6783d3a685c29fef5ecc4aad237c20c92452246b
Dan Habot <dan.habot@gmail.com>
2021-04-11 01:10:25 -0400

    use feed works

--- react-ruqqus/useFeed.ts
+++ react-ruqqus/useFeed.ts
@@ -5,8 +5,9 @@ import {useRuqqusFetch} from './useRuqqusFetch';
 export function useFeed(edge: string, args?: UseFetchOpts) {
   const [posts, setPosts] = useState<object[]>();
   const [page, setPage] = useState(1);
+  const [more, setMore] = useState(true);
 
-  const {loading, body} = useRuqqusFetch(`${edge}/listing`, {
+  const {loading, body, refresh} = useRuqqusFetch(`${edge}/listing`, {
     args: {
       ...args,
       page,
@@ -15,14 +16,16 @@ export function useFeed(edge: string, args?: UseFetchOpts) {
 
   useEffect(() => {
     if (body) {
-      setPosts((prev) => (page > 1 ? prev?.concat(body) : body));
+      let {data} = body;
+      setPosts((prev) => (page > 1 ? prev?.concat(data) : data));
+      setMore(body.next_exists);
     }
-  }, [page]);
+  }, [page, body]);
 
   return {
     loading,
     posts,
-    nextPage: () => setPage((x) => x + 1),
-    refresh: () => setPage(0),
+    nextPage: () => setPage((x) => (more ? x + 1 : x)),
+    refresh,
   };
 }
