9c055d4e27a766818696a20f636dbfb59d275c32
Dan Habot <dan.habot@gmail.com>
2021-04-16 11:41:48 -0400

    linked components

--- android/app/src/main/java/com/scrollforruqqus/MainApplication.java
+++ android/app/src/main/java/com/scrollforruqqus/MainApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.slider.ReactSliderPackage;
+import com.reactnativecommunity.checkbox.ReactCheckBoxPackage;
 import com.rnfs.RNFSPackage;
 import com.reactnativecommunity.webview.RNCWebViewPackage;
 import com.horcrux.svg.SvgPackage;
