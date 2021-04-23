007780b12990b91582f94594a5938c59185bab32
Dan Habot <dan.habot@gmail.com>
2021-04-23 00:04:30 -0400

    imporved network debugging

--- react-ruqqus/fetcher.ts
+++ react-ruqqus/fetcher.ts
@@ -1,7 +1,9 @@
-export interface fetcherOpts {
+import fetch from "unfetch";
+export interface fetcherOpts<T> {
   args?: Object;
-  body?: any;
+  body?: T;
   access_token?: string;
+  controller?: AbortController;
 }
 
 function seralize(obj: any): string {
@@ -10,7 +12,13 @@ function seralize(obj: any): string {
     .join("&");
 }
 
-export function fetcher(host: string, edge: string, opts: fetcherOpts = {}) {
+var FETCH_ID = 0;
+
+export function fetcher<T>(
+  host: string,
+  edge: string,
+  opts: fetcherOpts<T> = {},
+) {
   let args = seralize(opts.args);
   let reqbody = seralize(opts.body);
 
@@ -25,10 +33,17 @@ export function fetcher(host: string, edge: string, opts: fetcherOpts = {}) {
       "User-Agent": `scroll-for-ruqqus`,
       Authorization: `Bearer ${opts.access_token}`,
     },
-    // redirect: 'manual',
+    signal: opts.controller,
   };
 
-  console.log("RUQQUS FETCH", { host, edge, opts, options });
+  const fid = FETCH_ID++;
+  if (opts.controller) {
+    opts.controller.signal.onabort = () => {
+      console.log("RUQQUS FETCH ABORTED", host, edge, fid);
+    };
+  }
+
+  console.log("RUQQUS FETCH", { host, edge, opts, options, fid });
 
   return fetch(
     `https://${host}/${edge}${args ? "?" : ""}${args}`.toLowerCase(),
@@ -40,22 +55,29 @@ export function fetcher(host: string, edge: string, opts: fetcherOpts = {}) {
       );
       let b: Promise<any> = isObject ? r.json() : r.text();
 
-      return b.then((body) => ({
+      return b.then((body: T) => ({
         ...r,
         body,
       }));
     })
-    .then((r) => {
-      console.log("RUQQUS FETCH DONE", { host, edge, opts, options, r });
-      if (r.ok) {
-        return r;
+    .then((resp) => {
+      console.log("RUQQUS FETCH DONE", {
+        host,
+        edge,
+        opts,
+        options,
+        r: resp,
+        fid,
+      });
+      if (resp.ok) {
+        return resp;
       } else if (
-        typeof r.body === "object" &&
-        r.body["error"] == "401 Not Authorized. Invalid or Expired Token"
+        typeof resp.body === "object" &&
+        resp.body["error"] == "401 Not Authorized. Invalid or Expired Token"
       ) {
-        throw new Error("Login error: " + JSON.stringify(r));
+        throw new Error("Login error: " + JSON.stringify(resp));
       } else {
-        throw new Error("Client error: " + JSON.stringify(r));
+        throw new Error("Client error: " + JSON.stringify(resp));
       }
     });
 }
