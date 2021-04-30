1d3e6bef0141bbf3028d55cd3b0dfeaab49f4fc0
Dan Habot <dan.habot@gmail.com>
2021-04-30 17:43:21 -0400

    Started work on comment screen

--- views/comments.tsx
+++ views/comments.tsx
@@ -1,48 +1,76 @@
 import { useRoute } from "@react-navigation/native";
-import React, { useEffect } from "react";
-import { ActivityIndicator, View, Alert } from "react-native";
+import React, { useEffect, useContext, createContext } from "react";
+import {
+  ActivityIndicator,
+  View,
+  Alert,
+  ScrollView,
+  RefreshControl,
+} from "react-native";
 import WebView from "react-native-webview";
-import { useRuqqusClient } from "@react-ruqqus";
+import {
+  PostContext,
+  useRuqqusClient,
+  usePost,
+  RuqqusComment,
+} from "@react-ruqqus";
 import { useStyle, useTheme, useValue } from "@contexts";
+import { CardSelector } from "components/postcards/cardSelector";
+import { PopupWrapper } from "./PopupWrapper";
+import TextBox from "components/TextBox";
+import HtmlMarkdown from "components/HtmlMarkdown";
+
+const DepthContext = createContext(0);
+
+function Reply({ reply }: { reply: RuqqusComment }) {
+  const depth = useContext(DepthContext);
+  const theme = useTheme();
+
+  const SPACER = 2;
+
+  return (
+    <DepthContext.Provider value={depth + 1}>
+      <View
+        style={{
+          borderLeftColor: theme?.Colors.primary,
+          borderLeftWidth: SPACER,
+          marginLeft: 2 * SPACER * depth,
+          marginBottom: 2 * SPACER,
+        }}>
+        <HtmlMarkdown html={reply.body_html} />
+
+        {reply.replies.map((reply) => (
+          <Reply reply={reply} />
+        ))}
+      </View>
+    </DepthContext.Provider>
+  );
+}
 
 export default function Comments() {
   const route = useRoute<any>();
   const client = useRuqqusClient();
   const style = useStyle();
   const theme = useTheme();
-  const [appologise] = useValue<boolean>("general", "appologise");
-
-  useEffect(() => {
-    if (appologise) {
-      Alert.alert(
-        "Sorry",
-        "We're gonna have to use the webapp while we wait for the Ruqqus maintainers to implement a comments api. If you find this message annoying, please ask the maintainers to implement a comments api.",
-      );
-    }
-  }, []);
+  const { loading, body, refresh } = usePost(route.params.post_id);
 
   return (
-    <WebView
-      style={style?.view}
-      source={{
-        uri: `https://${client.domain}/post/${route?.params?.post_id}`,
-      }}
-      renderLoading={() => (
-        <View
-          style={{
-            position: "absolute",
-            top: 0,
-            alignContent: "center",
-            alignItems: "center",
-            alignSelf: "center",
-            padding: theme?.Space.get?.(1),
-          }}>
-          <ActivityIndicator color={theme?.Colors.primary} size="large" />
-        </View>
-      )}
-      javaScriptEnabled
-      startInLoadingState
-      domStorageEnabled
-    />
+    <ScrollView
+      style={style?.root}
+      refreshControl={
+        <RefreshControl refreshing={loading} onRefresh={() => refresh()} />
+      }>
+      {!loading && body ? (
+        <PostContext.Provider value={body}>
+          <PopupWrapper>
+            <CardSelector />
+          </PopupWrapper>
+        </PostContext.Provider>
+      ) : null}
+
+      {body?.replies.map((reply) => (
+        <Reply reply={reply} />
+      ))}
+    </ScrollView>
   );
 }
