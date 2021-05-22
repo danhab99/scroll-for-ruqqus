a43147f14519be2948a8cbec440936c85d2c602a
Dan Habot <dan.habot@gmail.com>
2021-05-22 13:52:58 -0400

    notifications screen

--- react-ruqqus/useNotifications.ts
+++ react-ruqqus/useNotifications.ts
@@ -2,7 +2,9 @@ import { RuqqusNotifications } from "./types";
 import { useRuqqusFetch } from "./useRuqqusFetch";
 
 export function useNotifications(all: boolean = false) {
-  return useRuqqusFetch<RuqqusNotifications>("notifications", {
-    args: all ? "true" : "false",
+  return useRuqqusFetch<{ data: RuqqusNotifications }>("notifications", {
+    args: {
+      all: all ? "True" : "",
+    },
   });
 }
--- views/notifications.tsx
+++ views/notifications.tsx
@@ -1,37 +1,97 @@
 import React, { useState, useEffect, createRef } from "react";
-import { Text, View, FlatList, RefreshControlComponent } from "react-native";
+import { Text, View, FlatList, RefreshControl, Pressable } from "react-native";
 import { useNavigation, useRoute } from "@react-navigation/core";
 import { useNotifications } from "react-ruqqus";
 import { useStyle, useTheme } from "@contexts";
 import * as _ from "lodash";
+import TimeAgo from "react-native-timeago";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
 import { StackNavigationProp } from "@react-navigation/stack";
 import { NoContent } from "./NoContent";
 import { RuqqusNotification } from "../react-ruqqus/types";
 
-function NotificationItem(props: { item: RuqqusNotification }) {
-  return <View></View>;
+import TextBox from "../components/TextBox";
+import HtmlMarkdown from "../components/HtmlMarkdown";
+
+function NotificationItem({ item }: { item: RuqqusNotification }) {
+  const style = useStyle();
+  const theme = useTheme();
+  const navigation = useNavigation<StackNavigationProp<any>>();
+  const route = useRoute();
+
+  return (
+    <Pressable
+      onPress={() =>
+        navigation.push("Comments", {
+          post_id: item.post.id,
+          lastRoute: route.name,
+        })
+      }>
+      <View
+        style={[
+          style?.card,
+          style?.paddedCard,
+          {
+            padding: theme?.Space.get?.(0.5),
+          },
+        ]}>
+        <TextBox>
+          New notification received <TimeAgo time={item.created_utc * 1000} />
+        </TextBox>
+        <TextBox>
+          <TextBox color="primary">@{item.author_name}</TextBox>{" "}
+          <Icon name="arrow-right-bold" /> {item.post.id}
+        </TextBox>
+
+        <HtmlMarkdown html={item.body_html} />
+      </View>
+    </Pressable>
+  );
 }
 
 export default function Notifications() {
   const navigation = useNavigation<StackNavigationProp<any>>();
-  const route = useRoute<any>();
   const style = useStyle();
   const theme = useTheme();
-  const { loading, body, refresh } = useNotifications();
+  const [all, setAll] = useState(false);
+  const { loading, body, refresh } = useNotifications(all);
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            paddingRight: theme?.Space.get?.(1),
+          }}>
+          <Pressable onPress={() => setAll((x) => !x)}>
+            <TextBox size={2}>
+              All{" "}
+              <Icon
+                size={theme?.FontSize.get?.(2)}
+                name={`checkbox-${all ? "marked" : "blank"}`}
+              />
+            </TextBox>
+          </Pressable>
+        </View>
+      ),
+    });
+  }, [all]);
 
   return (
     <View style={style?.root}>
       <FlatList
-        data={body || []}
+        data={body?.data || []}
         renderItem={({ item }) => <NotificationItem item={item} />}
-        ListEmptyComponent={<NoContent message="No new notifications" />}
+        ListEmptyComponent={() => <NoContent message="No new notifications" />}
         refreshControl={
-          <RefreshControlComponent
-            refreshing={loading}
-            onRefresh={() => refresh()}
-          />
+          <RefreshControl refreshing={loading} onRefresh={() => refresh()} />
         }
+        contentContainerStyle={{
+          marginTop: theme?.Space.get?.(1),
+        }}
       />
     </View>
   );
