03e8578058571f0ebd2bae6cda73c6f9fbebbbd3
Dan Habot <dan.habot@gmail.com>
2021-07-09 15:47:55 +0300

    debounce

--- react-ruqqus/useFeed.ts
+++ react-ruqqus/useFeed.ts
@@ -51,10 +51,16 @@ export function useFeed(edge: FeedOptions, args?: UseFeedOpts) {
 
   useEffect(() => setPage(1), [args?.sort]);
 
+  const nextPage = () => {
+    if (body?.next_exists && !loading) {
+      setPage((x) => x + 1);
+    }
+  };
+
   return {
     loading,
     posts: body?.data,
-    nextPage: () => (body?.next_exists ? setPage((x) => x + 1) : null),
+    nextPage,
     refresh,
     setPosts: setBody,
   };
