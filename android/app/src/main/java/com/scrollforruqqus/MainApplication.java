460ff9f08d2d3826859f1393896d0f1cb104404a
Dan Habot <dan.habot@gmail.com>
2021-07-28 09:56:51 -0400

    fixed gesture package

--- android/app/src/main/java/com/scrollforruqqus/MainApplication.java
+++ android/app/src/main/java/com/scrollforruqqus/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.reactnativecommunity.slider.ReactSliderPackage;
 import com.reactnativecommunity.checkbox.ReactCheckBoxPackage;
 import com.rnfs.RNFSPackage;
